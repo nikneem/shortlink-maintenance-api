de844461053e0f471d215cfb2729b8066b3aeb0a
nikneem <ekeilholz+git@outlook.com>
2020-09-28 12:04:19 +0200

    Issues/14 add caching (#17)
    * Add caching
    
    * Update core package
    
    * Using Cache
    
    * Changed redis cache template
    
    * Update exception handling
    
    Co-authored-by: Eduard Keilholz <e.keilholz@4dotnet.nl>

--- src/HexMaster.ShortLink.Resolver/Functions/ResolveShortCodeFunction.cs
+++ src/HexMaster.ShortLink.Resolver/Functions/ResolveShortCodeFunction.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using HexMaster.ShortLink.Core;
+using HexMaster.ShortLink.Core.Contracts;
 using HexMaster.ShortLink.Core.Models.Analytics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -13,15 +14,16 @@ using HexMaster.ShortLink.Core.Entities;
 
 namespace HexMaster.ShortLink.Resolver.Functions
 {
-    public static class ResolveShortCodeFunction
+    public class ResolveShortCodeFunction
     {
+        private readonly IShortLinksService _shortLinksService;
+
         [FunctionName("ResolveShortCodeFunction")]
-        public static async Task<IActionResult> ResolveShortCode(
+        public async Task<IActionResult> ResolveShortCode(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{*path}")]
             HttpRequest req,
             [EventHub(HubNames.ClickEventsHub, Connection = "CloudSettings:EventHubSenderConnectionString")]
             IAsyncCollector<LinkClickedMessage> outputEvents,
-            [Table(TableNames.ShortLinks)] CloudTable table,
             string path,
             ILogger log)
         {
@@ -29,7 +31,7 @@ namespace HexMaster.ShortLink.Resolver.Functions
             var now = DateTimeOffset.UtcNow;
             if (!string.IsNullOrWhiteSpace(path))
             {
-                var targetEndpoint = await GetShortLinkEntity(table, path, log);
+                var targetEndpoint = await _shortLinksService.ResolveAsync(path);
                 if (!string.IsNullOrWhiteSpace(targetEndpoint))
                 {
                     targetUrl = targetEndpoint;
@@ -44,6 +46,11 @@ namespace HexMaster.ShortLink.Resolver.Functions
 
             await outputEvents.FlushAsync();
             return new RedirectResult(targetUrl, true);
+        }
+
+        public ResolveShortCodeFunction(IShortLinksService shortLinksService)
+        {
+            _shortLinksService = shortLinksService;
         }
 
         private static async Task<string> GetShortLinkEntity(CloudTable table, string path, ILogger log)
