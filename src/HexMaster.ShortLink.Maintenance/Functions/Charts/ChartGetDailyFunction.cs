90bc4052ae1a668612635048a4c9944889b76d20
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-10-05 14:30:57 +0200

    Fixed graphs

--- src/HexMaster.ShortLink.Maintenance/Functions/Charts/ChartGetDailyFunction.cs
+++ src/HexMaster.ShortLink.Maintenance/Functions/Charts/ChartGetDailyFunction.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HexMaster.ShortLink.Core.Charts.Contracts;
@@ -7,9 +6,7 @@ using HexMaster.ShortLink.Core.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace HexMaster.ShortLink.Maintenance.Functions.Charts
 {
