240f9f98c5e1e8e65e3db15356e34af76fc5f084
nikneem <ekeilholz+git@outlook.com>
2020-10-23 13:27:14 +0200

    #31 - Add endpoint for removal of shortlinks (#33)
    Co-authored-by: Eduard Keilholz <e.keilholz@4dotnet.nl>

---
+++ src/HexMaster.ShortLink.Maintenance/Functions/ShortLinks/ShortLinkDeleteFunction.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using HexMaster.Functions.JwtBinding;
+using HexMaster.Functions.JwtBinding.Model;
+using HexMaster.ShortLink.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HexMaster.ShortLink.Maintenance.Functions.ShortLinks
+{
+    public  class ShortLinkDeleteFunction
+    {
+        private readonly IShortLinksService _service;
+        private readonly ILogger<ShortLinkCreateFunction> _logger;
+
+        [FunctionName("ShortLinkDeleteFunction")]
+        public  async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "shortlink/{id:guid}")] HttpRequest req,
+            Guid id,
+            [JwtBinding] AuthorizedModel auth)
+        {
+            _logger.LogInformation("Incoming request for deletion");
+            try
+            {
+                await _service.DeleteAsync(auth.Subject, id);
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to remove shortlink from system");
+            }
+            return new BadRequestResult();
+        }
+
+        public ShortLinkDeleteFunction(IShortLinksService service,
+            ILogger<ShortLinkCreateFunction> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+    }
+}
